8bac0c3d8151a863734e5f268cee0bb5b9b515e5
irmenj <irmenj@rose-hulman.edu>
2025-09-25 09:00:47 -0400

    resume

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
 
-    let button = document.getElementById("completedCoursed");
-    courseId = ["course1","course2","course3","course4","course5"];
-    i=0;
-    setInterval(function(){
+  
+  setInterval(changeButtonText, 2000);
+    function changeButtonText(){
+        let button = document.getElementById("completedCoursed");
+        courseId = ["course1","course2","course3","course4","course5"];
+        i=0;
         button.innerHTML=courseId[i++].innerHTML;
         i=courseId.length === 1? 0:1;
-    },2000);
-    
+    }
+  
