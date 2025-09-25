c1d6a5b3fc4b5e1ddc42fe59527028e6b2f1f4b8
irmenj <irmenj@rose-hulman.edu>
2025-09-24 23:43:45 -0400

    added some javaScript

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
-function compltedCoursed(){
+
     let button = document.getElementById("completedCoursed");
     courseId = ["course1","course2","course3","course4","course5"];
-    button.addEventListener("click", function(){
-            
-    });
-}
+    i=0;
+    setInterval(function(){
+        button.innerHTML=courseId[i++].innerHTML;
+        i=courseId.length === 1? 0:1;
+    },2000);
+    
