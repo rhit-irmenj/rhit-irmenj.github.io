a70c7ea030a2a59960fca135dc355f1c904d7eb8
irmenj <irmenj@rose-hulman.edu>
2025-09-24 23:35:59 -0400

    working on JavaScript

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function compltedCoursed(){
+    let button = document.getElementById("completedCoursed");
+    courseId = ["course1","course2","course3","course4","course5"];
+    button.addEventListener("click", function(){
+            
+    });
+}
