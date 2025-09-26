60b3a21cce91e3a6ea0aba3a745d1a4c77c0c29a
irmenj <irmenj@rose-hulman.edu>
2025-09-26 07:19:16 -0400

    scroll button

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
 
    let i=0;
+    let button1 = document.getElementById("courseButton1");
+    let button2 = document.getElementById("courseButton2");
    function changeButtonText(){
-        let button1 = document.getElementById("courseButton1");
-        let button2 = document.getElementById("courseButton2");
+
         completedId = ["completed1","completed2","completed3","completed4","completed5"];
         notCompletedID = ["not_complete1","not_complete2","not_complete3","not_complete4","not_complete5"];
 
@@ -14,10 +15,17 @@
         if( i >= completedId.length ){
             i = 0;
         }
-       
-    }
-    
 
+    }
+    let scrollButton = document.getElementById("toTop");
+    scrollButton.addEventListener("click", () => {
+        window.scrollTo({
+            top:0,
+            behavior:"smooth"
+
+        })
+    });
+   
     function navBar(){
               
             fetch("nav.html")
